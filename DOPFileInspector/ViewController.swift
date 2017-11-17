745f1c6958bdc92c82906aec193fa15047e0454d
Feng Weizhou <shiwoow@gmail.com>
2017-11-17 22:24:20 +0800

    Initial Commit

---
+++ DOPFileInspector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DOPFileInspector
+//
+//  Created by 纬洲 冯 on 17/11/2017.
+//  Copyright © 2017 fengweizhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
